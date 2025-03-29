2bc7fddf5889bf8888e23f882cd346bfcb09ef04
wolfgang <wolfgang.yin@gmail.com>
2025-03-29 20:11:01 +0800

    use zig cc as CC

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 使用系统 clang
-CC="clang" \
+CC="zig cc" \
 CGO_ENABLED=1 \
 CGO_LDFLAGS="-L$(pwd)/zig-out/lib -lcgo_static_linking" \
 GOOS=darwin GOARCH=arm64 \
-go build -a main.go
+go build -ldflags="-s" main.go
